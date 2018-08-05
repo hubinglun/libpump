02d91775ffd8f19e413e78149cbf904329c8ab91
yz <263693992@qq.com>
2018-08-05 18:56:36 +0800

    add: 加入Event基本派生

--- src/Event.h
+++ src/Event.h
@@ -11,18 +11,160 @@
 #ifndef LIBPUMP_EVENT_H
 #define LIBPUMP_EVENT_H
 
+#include <string>
+
 #include "pumpdef.h"
+#include "CbMailbox.h"
+
+namespace nsp_std = ::std;
 
 namespace PUMP {
 
-class Event {
-private:
-  //! 描述事件的优先级
-  enum EventPriority m_evPriority;
-  //! 事件的状态
-  enum EventState m_evState;
-  //! 事件的类型，事前、普通、事后
-  enum EventType m_evType;
+/**
+ * @struct Event []
+ * @brief 事件对象的抽象类
+ */
+PUMP_ABSTRACT
+struct Event
+  : public nsp_boost::noncopyable {
+  //! < 事件命
+  nsp_std::string strName_;
+  //! < 描述事件的优先级
+  enum EventPriority emEvPriority_;
+  //! < 事件的状态
+  enum EventState emEvState_;
+  /**
+   * @var const enum EventType m_emEvType
+   * @brief 事件的类型: 前置, 普通, 后置
+   */
+  const enum EventType emEvType_;
+  /**
+   * @var PtrCbFn m_pPreEvCallback
+   * @brief 指向前置事件回调
+   *
+   * 由Event事件派生对象的构造者指定
+   */
+  PtrCbFn pPreEvCallback_;
+  /**
+   * @var PtrCbFn m_pEvCallback
+   * @brief 指向事件回调
+   *
+   * 由Event事件派生对象的构造者指定
+   */
+  PtrCbFn pEvCallback_;
+  /**
+   * @var PtrCbFn m_pPostEvCallback
+   * @brief 指向后置事件回调
+   *
+   * 由Event事件派生对象的构造者指定
+   */
+  PtrCbFn pPostEvCallback_;
+  /**
+   * @var PtrArg m_pData
+   * @brief 指向用户数据
+   *
+   * 由Event事件派生对象的构造者指定
+   */
+  PtrArg pData_;
+public:
+  Event();
+  
+  Event(const enum EventType emEvType);
+  
+  virtual ~Event() {}
+};
+
+/**
+ * @typedef typedef nsp_boost::shared_ptr<Event> PtrEvent
+ * @brief Event 的内存托管
+ */
+typedef nsp_boost::shared_ptr<Event> PtrEvent;
+
+/**
+* @struct PreEvent []
+* @brief 前置事件
+*/
+PUMP_ABSTRACT
+struct PreEvent
+  : public Event {
+  PreEvent();
+  
+  virtual ~PreEvent();
+};
+
+/**
+ * @struct PostEvent []
+ * @brief
+ */
+PUMP_ABSTRACT
+struct PostEvent
+  : public Event {
+  PostEvent();
+  
+  virtual ~PostEvent();
+};
+
+/**
+ * @struct NormEvent []
+ * @brief
+ */
+PUMP_ABSTRACT
+struct NormEvent
+  : public Event {
+  NormEvent();
+  
+  virtual ~NormEvent();
+};
+
+/**
+ * @struct FD_T []
+ * @brief 文件描述符对象
+ */
+struct FD_T {
+  pump_fd_t fd_;
+};
+
+/**
+ * @struct FdEvent []
+ * @brief
+ */
+PUMP_ABSTRACT
+struct FdEvent
+  : public NormEvent {
+  FD_T fd_;
+  
+  FdEvent();
+  
+  virtual ~FdEvent();
+  
+  /**
+   * @fn virtual void init() = 0
+   * @brief 初始化 fd_ 成员
+   */
+  virtual void init() = 0;
+};
+
+struct IoEvent
+  : public FdEvent {
+  virtual void init();
+};
+
+struct Timestamp {
+
+};
+
+struct TimerEvent
+  : public FdEvent {
+  Timestamp m_tsInit;
+  Timestamp m_tsTimeout;
+  enum TimerType;
+  
+  virtual void init();
+};
+
+struct SignalEvent
+  : public FdEvent {
+  virtual void init();
 };
 
 }
