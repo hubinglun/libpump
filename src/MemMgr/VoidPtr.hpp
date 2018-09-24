3ce4323efc1fcaadd932c4fb6a0e279ad588af84
YangZheng <263693992@qq.com>
2018-09-24 21:20:37 +0800

    [NEW] 指向原始内存的一级智能指针对象
    本对象是整个内存管理解决方案的基础对象, 其封装原始指针的操作, 用户将可以像操作裸指针一样操作此对象

--- src/MemMgr/VoidPtr.hpp
+++ src/MemMgr/VoidPtr.hpp
@@ -5,218 +5,409 @@
 #include <cassert>
 #include <iostream>
 
+//#include <boost/smart_ptr/detail/sp_counted_base.hpp>
+//#include <boost/smart_ptr/detail/sp_counted_impl.hpp>
+
 #include "SmartPtr.h"
+#include "SharedCount.hpp"
+
+namespace nsp_boost = boost;
 
 namespace Pump {
 namespace MemMgr {
 
-// FIXME 这个对象非常关键, 需要解决多线程同步问题
-class _shared_count {
-public:
-  _shared_count() {
-    use_count_ = 1;
-  }
-  
-  void add_ref_copy() {
-    // FIXME 加锁
-    ++use_count_;
-  }
-  
-  void release() {
-    // FIXME 加锁
-    --use_count_;
-  }
-  
-  long use_count() const {
-    return static_cast<long const volatile &>(use_count_);
-  }
-  
-  template<class _A>
-  friend bool swap(_shared_count **left, _shared_count **right, _A &alloc);
+const unsigned short MEM_PACK_LEN = 8;
 
-private:
-  volatile long use_count_; // #shared
+//#pragma pack(push)
+//#pragma pack(MEM_PACK_LEN)
+
+class _Del {
+public:
+  virtual void operator()(void *p, size_t iSize, std::allocator<char> &a) const {
+    a.deallocate((typename std::allocator<char>::pointer) p, iSize);
+  }
 };
 
-template<class _A>
-bool swap(_shared_count **left, _shared_count **right, _A &alloc) {
-  // FIXME 加锁
-  bool ret = false;
-  if ((*right) == nullptr) {
-    return ret;
-  }
-  (*right)->add_ref_copy();
-  if ((*left) == nullptr) {
-    (*left) = (*right);
-    return ret;
-  }
-  (*right)->release();
-  if ((*left)->use_count() == 0) {
-    alloc.deallocate((char *) (*left), sizeof(_shared_count));
-    ret = true;
-  }
-  (*left) = (*right);
-  return ret;
-}
+enum RelativeType {
+  RELATIVE_INTERSECT,
+  RELATIVE_INCLUDE,
+  RELATIVE_NONE,
+  RELATIVE_EQUAL,
+};
 
-template<class _A> class VoidPtr;
+class VoidPtr;
 
-template<class _A>
-bool operator==(const VoidPtr<_A> &, std::nullptr_t);
+bool operator==(VoidPtr const &p, boost::detail::sp_nullptr_t) BOOST_NOEXCEPT;
 
-template<class _A>
-bool operator==(std::nullptr_t, const VoidPtr<_A> &);
+bool operator==(boost::detail::sp_nullptr_t, VoidPtr const &p) BOOST_NOEXCEPT;
 
-template<class _A = std::allocator<char> >
+bool operator!=(VoidPtr const &p, boost::detail::sp_nullptr_t) BOOST_NOEXCEPT;
+
+bool operator!=(boost::detail::sp_nullptr_t, VoidPtr const &p) BOOST_NOEXCEPT;
+
+bool operator==(VoidPtr const &a, VoidPtr const &b) BOOST_NOEXCEPT;
+
+bool operator!=(VoidPtr const &a, VoidPtr const &b) BOOST_NOEXCEPT;
+
+std::ostream &operator<<(std::ostream &os, VoidPtr const &p);
+
 class VoidPtr {
+private:
+  typedef VoidPtr this_type;
 public:
-  typedef _A allocator_t;
-public:
-  VoidPtr()
-    : m_alloc(getAlloc()),
-      m_iCopacity(0),
-      m_iSize(0),
-      m_pn(nullptr),
-      m_px(nullptr) {
+  VoidPtr() BOOST_NOEXCEPT
+    : /*m_alloc(getAlloc()),*/
+    m_iCapacity(0),
+    m_iSize(0),
+    m_px(0),
+    m_pn() {
     
   }
+
+#ifndef BOOST_NO_CXX11_NULLPTR
   
-  explicit VoidPtr(allocator_t &alloc)
-    : m_alloc(alloc),
-      m_iCopacity(0),
-      m_iSize(0),
-      m_pn(nullptr),
-      m_px(nullptr) {
+  VoidPtr(boost::detail::sp_nullptr_t) BOOST_NOEXCEPT
+    : /*m_alloc(getAlloc()),*/
+    m_iCapacity(0),
+    m_iSize(0),
+    m_px(0),
+    m_pn() {
     
   }
+
+#endif //BOOST_NO_CXX11_NULLPTR
   
-  explicit VoidPtr(allocator_t &alloc, const size_t size)
-    : m_alloc(alloc),
-      m_iCopacity(size),
-      m_iSize(0),
-      m_px(nullptr) {
-    m_pn = (_shared_count *) alloc.allocate(sizeof(_shared_count));
-    ::new(m_pn)_shared_count();
-    m_px = (void *) alloc.allocate(size);
+  template<class A = std::allocator<char> >
+  explicit VoidPtr(A &a, const size_t size) BOOST_NOEXCEPT
+    : /*m_alloc(a),*/
+    m_iCapacity(size),
+    m_iSize(0),
+    m_px(0),
+    m_pn() {
+    // FIXME [general] std::allocator<T> 分配实际空间为(sizeof(T) * size)
+    m_px = (void *) a.allocate(size);
     ::memset(m_px, 0, size);
+    /** FIXME [urgency] [fixed] shared_count不支持仅输入构造指针和分配器的构造函数
+     * 重写了 SharedCount 对象
+     */
+    m_pn = SharedCount(m_px, m_iCapacity, a);
   }
   
-  VoidPtr(allocator_t &alloc, void *px, const size_t size)
-    : m_alloc(alloc),
-      m_iCopacity(size),
-      m_iSize(0),
-      m_pn(nullptr),
-      m_px(px) {
-    m_pn = (_shared_count *) alloc.allocate(sizeof(_shared_count));
-    ::new(m_pn)_shared_count();
+  explicit VoidPtr(const size_t size) BOOST_NOEXCEPT
+    : /*m_alloc(a),*/
+    m_iCapacity(size),
+    m_iSize(0),
+    m_px(0),
+    m_pn() {
+    // FIXME [general] std::allocator<T> 分配实际空间为(sizeof(T) * size)
+    m_px = (void *) getAlloc().allocate(size);
+    ::memset(m_px, 0, size);
+    /** FIXME [urgency] [fixed] shared_count不支持仅输入构造指针和分配器的构造函数
+     * 重写了 SharedCount 对象
+     */
+    m_pn = SharedCount(m_px, m_iCapacity, getAlloc());
   }
   
-  VoidPtr(VoidPtr &other)
-    : m_alloc(other.m_alloc),
-      m_iCopacity(other.m_iCopacity),
-      m_iSize(other.m_iSize),
-      m_px(nullptr),
-      m_pn(nullptr) {
+  template<class A = std::allocator<char>, class D>
+  explicit VoidPtr(A &a, const size_t size, D del) BOOST_NOEXCEPT
+    : /*m_alloc(a),*/
+    m_iCapacity(size),
+    m_iSize(0),
+    m_px(0),
+    m_pn() {
+    // FIXME [general] std::allocator<T> 分配实际空间为(sizeof(T) * size)
+    m_px = (void *) a.allocate(size);
+    ::memset(m_px, 0, size);
+    /** FIXME [urgency] [fixed] shared_count不支持仅输入构造指针和分配器的构造函数
+     * 重写了 SharedCount 对象
+     */
+    m_pn = SharedCount(m_px, m_iCapacity, a, del);
+  }
+  
+  template<class D>
+  explicit VoidPtr(const size_t size, D del) BOOST_NOEXCEPT
+    : /*m_alloc(a),*/
+    m_iCapacity(size),
+    m_iSize(0),
+    m_px(0),
+    m_pn() {
+    // FIXME [general] std::allocator<T> 分配实际空间为(sizeof(T) * size)
+    m_px = (void *) getAlloc().allocate(size);
+    ::memset(m_px, 0, size);
+    /** FIXME [urgency] [fixed] shared_count不支持仅输入构造指针和分配器的构造函数
+     * 重写了 SharedCount 对象
+     */
+    m_pn = SharedCount(m_px, m_iCapacity, getAlloc(), del);
+  }
+  
+  template<class A = std::allocator<char> >
+  VoidPtr(A &a, void *px, const size_t size) BOOST_NOEXCEPT
+    : /*m_alloc(a),*/
+    m_iCapacity(size),
+    m_iSize(0),
+    m_px(px),
+    m_pn() {
+    m_pn = SharedCount(m_px, m_iCapacity, a);
+  }
+  
+  VoidPtr(void *px, const size_t size) BOOST_NOEXCEPT
+    : /*m_alloc(a),*/
+    m_iCapacity(size),
+    m_iSize(0),
+    m_px(px),
+    m_pn() {
+    m_pn = SharedCount(m_px, m_iCapacity, getAlloc());
+  }
+  
+  template<class A = std::allocator<char>, class D>
+  VoidPtr(A &a, void *px, const size_t size, D del) BOOST_NOEXCEPT
+    : /*m_alloc(a),*/
+    m_iCapacity(size),
+    m_iSize(0),
+    m_px(px),
+    m_pn() {
+    m_pn = SharedCount(m_px, m_iCapacity, a, del);
+  }
+  
+  template<class D>
+  VoidPtr(void *px, const size_t size, D del) BOOST_NOEXCEPT
+    : /*m_alloc(a),*/
+    m_iCapacity(size),
+    m_iSize(0),
+    m_px(px),
+    m_pn() {
+    m_pn = SharedCount(m_px, m_iCapacity, getAlloc(), del);
+  }
+  
+  VoidPtr(VoidPtr const &r) BOOST_NOEXCEPT
+    : /*m_alloc(r.m_alloc),*/
+    m_iCapacity(r.m_iCapacity),
+    m_iSize(r.m_iSize),
+    m_px(r.m_px),
+    m_pn(r.m_pn) {
+  }
+
+#ifndef BOOST_NO_CXX11_RVALUE_REFERENCES
+  
+  VoidPtr(VoidPtr &&r) BOOST_NOEXCEPT
+    : /*m_alloc(r.m_alloc),*/
+    m_iCapacity(r.m_iCapacity),
+    m_iSize(r.m_iSize),
+    m_px(r.m_px),
+    m_pn() {
+    m_pn.swap(r.m_pn);
+    r.m_px = nullptr;
+  }
+
+#endif //BOOST_NO_CXX11_RVALUE_REFERENCES
+  
+  virtual ~VoidPtr() {
 #ifdef _TEST_LEVEL_INFO
-    assert(m_iCopacity >= m_iSize);
+    LOG(INFO) << "~VoidPtr()";
 #endif //_TEST_LEVEL_INFO
-    if (swap(&this->m_pn, &other.m_pn, m_alloc)/* == true*/) {
-      m_alloc.deallocate((char *) m_px, m_iCopacity);
-    }
-    m_alloc.deallocate((char *) m_pn, sizeof(_shared_count));
   }
+
+#ifndef BOOST_NO_CXX11_RVALUE_REFERENCES
   
-  virtual void release() {
-    if (m_pn == nullptr) {
-      return;
-    }
-    m_pn->release();
-    if (m_pn->use_count() == 0) {
-      if (m_iCopacity > 0) {
-        m_alloc.deallocate((char *) m_px, m_iCopacity);
-        m_px = nullptr;
-        m_iCopacity = 0;
-      }
-      m_alloc.deallocate((char *) m_pn, sizeof(_shared_count));
-      m_pn = nullptr;
-    }
-  }
-  
-  VoidPtr &operator=(VoidPtr &other) {
-    m_iCopacity = other.m_iCopacity;
-    m_iSize = other.m_iSize;
-#ifdef _TEST_LEVEL_INFO
-    assert(m_iCopacity >= m_iSize);
-#endif //_TEST_LEVEL_INFO
-    if (swap(&this->m_pn, &other.m_pn, m_alloc)/* == true*/) {
-      m_alloc.deallocate((char *) m_px, m_iCopacity);
-    }
-    m_px = other.m_px;
+  VoidPtr &operator=(VoidPtr &&r) BOOST_NOEXCEPT {
+#ifdef _TEST_LEVEL_DEBUG
+    assert(m_iCapacity >= m_iSize);
+#endif //_TEST_LEVEL_DEBUG
+    this_type(static_cast< VoidPtr && >(r)).swap(*this);
     return *this;
   }
+
+#endif //BOOST_NO_CXX11_RVALUE_REFERENCES
   
-  template<class _Y>
-  VoidPtr &operator=(const _Y &right) {
-    size_t iSize = sizeof(_Y);
-    if (iSize > this->m_iCopacity) {
-      // FIXME 抛出异常, 或者其他识别错误的方案
-      throw 1;
-    }
-    ::memset(m_px, 0, m_iCopacity);
-    ::memcpy(m_px, &right, iSize);
+  VoidPtr &operator=(VoidPtr const &r) BOOST_NOEXCEPT {
+#ifdef _TEST_LEVEL_DEBUG
+    assert(m_iCapacity >= m_iSize);
+#endif //_TEST_LEVEL_DEBUG
+    this_type(r).swap(*this);
     return *this;
   }
-  
-  template<class _Y>
-  _Y *get() {
-    if (sizeof(_Y) > this->m_iCopacity) {
-      // FIXME 抛出异常, 或者其他识别错误的方案
-      throw 1;
-    }
-    return (static_cast<_Y *>(m_px));
-  }
-  
-  template<class _Y>
-  const _Y *cget() const {
-    if (sizeof(_Y) > this->m_iCopacity) {
-      // FIXME 抛出异常, 或者其他识别错误的方案
-      throw 1;
-    }
-    return (static_cast<const _Y *>(m_px));
-  }
-  
-  // FIXME 需要一个功能更加强大的元素访问接口,应该能支持多维数组
-//  template<class _Y>
-//  _Y &operator[](size_t indx) {
-//
+
+//  // [deprecate] 直接内存拷贝不符合智能指针标准, 因为复制内容可能包含智能指针, 直接拷贝不会增加引用计数
+//  template<class T>
+//  VoidPtr &operator=(const T &right) {
+//    size_t iSize = sizeof(T);
+//    if (iSize > this->m_iCapacity) {
+//      // FIXME 抛出异常, 或者其他识别错误的方案
+//      throw 1;
+//    }
+//    ::memset(m_px, 0, m_iCapacity);
+//    ::memcpy(m_px, &right, iSize);
+//    return *this;
 //  }
   
-//  template<class _A_0>
-  friend bool operator==<_A>(const VoidPtr<_A> &p, std::nullptr_t);
+  void reset() BOOST_NOEXCEPT {
+    this_type().swap(*this);
+  }
   
-//  template<class _A_0>
-  friend bool operator==<_A>(std::nullptr_t, const VoidPtr<_A> &p);
+  template<class A>
+  void reset(A &a, void *p, const size_t iSize) // Y must be complete
+  {
+#ifdef _TEST_LEVEL_DEBUG
+    BOOST_ASSERT(p == 0 || p != m_px); // catch self-reset errors
+#endif //_TEST_LEVEL_DEBUG
+    this_type(a, p, iSize).swap(*this);
+  }
+  
+  template<class A, class D>
+  void reset(A &a, void *p, const size_t iSize, D d) {
+    this_type(a, p, iSize, d).swap(*this);
+  }
+  
+  void reset(VoidPtr &other) {
+    other.swap(*this);
+  }
+  
+  size_t capacity() const {
+    return m_iCapacity;
+  }
+  
+  size_t size() const {
+    return m_iSize;
+  }
+  
+  void swap(VoidPtr &r) BOOST_NOEXCEPT {
+    std::swap(m_iCapacity, r.m_iCapacity);
+    std::swap(m_iSize, r.m_iSize);
+    std::swap(m_px, r.m_px);
+    m_pn.swap(r.m_pn);
+  }
+  
+  long use_count() const // nothrow
+  {
+    return m_pn.use_count();
+  }
+  
+  bool unique() const BOOST_NOEXCEPT {
+    return m_pn.unique();
+  }
+  
+  enum RelativeType relative(VoidPtr const &r) const {
+    if (*this == nullptr || r == nullptr){
+      return RELATIVE_NONE;
+    }
+    if(m_px==r.m_px && (char*)m_px+m_iCapacity == (char*)r.m_px + r.m_iCapacity) {
+      return RELATIVE_EQUAL;
+    }
+    else if((m_px <= r.m_px && (char*)m_px+m_iCapacity >= (char*)r.m_px + r.m_iCapacity)
+            ||(m_px >= r.m_px && (char*)m_px+m_iCapacity <= (char*)r.m_px + r.m_iCapacity)) {
+      return RELATIVE_INCLUDE;
+    }
+    else{
+      return RELATIVE_INTERSECT;
+    }
+  }
+  
+  template<class T>
+  T *get() const {
+    if (sizeof(T) > this->m_iCapacity) {
+      // FIXME 抛出异常, 或者其他识别错误的方案
+      throw 1;
+    }
+    return (static_cast<T *>(m_px));
+  }
+  
+  void *get() const {
+    return (m_px);
+  }
 
 protected:
-  allocator_t m_alloc;
-  size_t m_iCopacity;
+  template<class T>
+  T *rget() {
+    if (sizeof(T) > this->m_iCapacity) {
+      // FIXME 抛出异常, 或者其他识别错误的方案
+      throw 1;
+    }
+    return (static_cast<T *>(m_px));
+  }
+
+public:
+  // FIXME 需要一个功能更加强大的元素访问接口,应该能支持多维数组
+//  template<class T>
+//  T &operator[](size_t indx) {
+//
+//  }
+
+protected:
+  size_t m_iCapacity;
   size_t m_iSize;
-  _shared_count *m_pn;
   void *m_px;
+  SharedCount m_pn;
 };
 
-template<typename _A>
-bool operator==(const VoidPtr<_A> &p, std::nullptr_t) {
-#ifdef _TEST_LEVEL_INFO
-  LOG(INFO) << typeid(_A).name();
-#endif //_TEST_LEVEL_INFO
-  return (p.m_px == nullptr);
+//template<class E, class T, class Y>
+//std::basic_ostream<E, T> & operator<< (std::basic_ostream<E, T> & os, VoidPtr const & p) {
+//  os << "[" << std::endl;
+//  if (p == nullptr) {
+//    os << "]" << std::endl;
+//    return os;
+//  }
+//  unsigned char *szBegin = p.get<unsigned char>();
+//  for (size_t i = 0, j = 0; i < p.capacity(); ++i) {
+//    unsigned char ch = *szBegin;
+//    if (j == 0) {
+//      os << "0x" << std::hex << std::setw(8) << std::setfill('0') << static_cast<unsigned long>(szBegin);
+//    }
+//    os << std::hex << std::setw(8) << std::setfill('0') << static_cast<short>(ch)
+//       << (j == MEM_PACK_LEN - 1 ? "\n" : "");
+//    j = (j == MEM_PACK_LEN - 1 ? 0 : j + 1);
+//    szBegin++;
+//  }
+//  return os;
+//}
+
+inline bool operator==(VoidPtr const &p, boost::detail::sp_nullptr_t) BOOST_NOEXCEPT {
+  return (p.get() == 0);
 }
 
-template<typename _A>
-bool operator==(std::nullptr_t, const VoidPtr<_A> &p) {
-  return (p.m_px == nullptr);
+inline bool operator==(boost::detail::sp_nullptr_t, VoidPtr const &p) BOOST_NOEXCEPT {
+  return (p.get() == 0);
 }
+
+inline bool operator!=(VoidPtr const &p, boost::detail::sp_nullptr_t) BOOST_NOEXCEPT {
+  return (p.get() != 0);
+}
+
+inline bool operator!=(boost::detail::sp_nullptr_t, VoidPtr const &p) BOOST_NOEXCEPT {
+  return (p.get() != 0);
+}
+
+inline bool operator==(VoidPtr const &a, VoidPtr const &b) BOOST_NOEXCEPT {
+  return (a.relative(b) == RELATIVE_EQUAL);
+}
+
+inline bool operator!=(VoidPtr const &a, VoidPtr const &b) BOOST_NOEXCEPT {
+  return (a.relative(b) != RELATIVE_EQUAL);
+}
+
+std::ostream &operator<<(std::ostream &os, VoidPtr const &p) {
+  os << "[" << std::endl;
+  if (p == nullptr) {
+    os << "]" << std::endl;
+    return os;
+  }
+  nsp_std::stringstream ss;
+  unsigned char *szBegin = p.get<unsigned char>();
+  for (size_t i = 0, j = 0; i < p.capacity(); ++i) {
+    unsigned char ch = *szBegin;
+    if (j == 0) {
+      ss << " 0x" << std::hex << std::setw(4) << std::setfill('0') << (unsigned long) (szBegin) << " ";
+    }
+    ss << std::hex << std::setw(4) << std::setfill('0') << static_cast<short>(ch)
+       << (j == MEM_PACK_LEN - 1 ? "\n" : " ");
+    j = (j == MEM_PACK_LEN - 1 ? 0 : j + 1);
+    szBegin++;
+  }
+  ss << "\n]" << std::endl;
+  os << ss.str();
+  return os;
+}
+
+//#pragma pack (pop)
 
 }
 }
